29e8a0a35f66b768fce7d4aed80a14d60dfc3565
Huong <you@example.com>
2018-02-27 09:58:12 +0700

    vue cli and first component

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ new Vue({
     data: {
         name: "Huong",
         age: 22,
-        message: 'You loaded this page on ' + new Date().toLocaleString()
+        message: 'You loaded this page on ' + new Date().toLocaleString(),
+        website: "https://www.facebook.com/huongnguyen.nh",
+        inputTag: `<input value="Hihi"/>`
     },
     methods: {
         hello: function (time) {
