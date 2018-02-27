ae1a8b24e0872c8552cf6a275fbf3039d0264001
Huong <you@example.com>
2018-02-27 10:12:30 +0700

    vue cli

---
+++ src/components/First.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>First Component</h1>
+        <h3>{{hello('morning')}}</h3>
+        <p>Name: {{name}}</p>
+        <p>Age: {{age}}</p>
+        <p>{{message}}</p>
+        <div>
+            <a v-bind:href="website">About me</a>
+        </div>
+        <div>
+            <input v-bind:value="name"/>
+        </div>
+        <div v-html="inputTag"></div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Huong",
+      age: 22,
+      message: "You loaded this page on " + new Date().toLocaleString(),
+      website: "https://www.facebook.com/huongnguyen.nh",
+      inputTag: `<input value="Hihi"/>`
+    };
+  },
+  methods: {
+    hello: function(time) {
+      return `Good ${time}, ${this.name}`;
+    }
+  }
+};
+</script>
